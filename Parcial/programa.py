944e6e60d70dfa2d6bd499625d9a3351c7ce8c84
Andrés Felipe López Pinto <102700999+10Andres10@users.noreply.github.com>
2022-12-01 12:42:48 -0500

    Update programa.py

--- Parcial/programa.py
+++ Parcial/programa.py
@@ -3,33 +3,62 @@ from tkinter import Tk, Label
 from tkinter import ttk
 import random
 
-filas = 3
-columnas = 3
+COLORS = [
+    "red",
+    "blue",
+    "yellow",
+    "orange",
+    "purple",
+    "green",
+    "white",
+    "gold",
+    "silver",
+]
 
 ventana = Tk()
 ventana.geometry("500x500")
 ventana.title("Matríz")
-
-numero = ttk.Entry(justify=tk.RIGHT)
-# numero.pack()
-
-for fila in range(filas):
-    ventana.rowconfigure(fila, weight=1)
-    for columna in range(columnas):
-        etiqueta = Label(text=random.randint(0, 9), bg="cadetblue1",font=1)
-        etiqueta.configure(font=99)
-        etiqueta.grid(row=fila, column=columna, padx=2, pady=2, sticky="nsew")
-        ventana.columnconfigure(columna, weight=1)
+ventana.configure(background="white")
 
 
-def encontrar_numero():
-    for i in range(0, 9):
+def crear_matriz(entry_numero):
+    filas = entry_numero
+    columnas = entry_numero
+    for fila in range(filas):
+        ventana.rowconfigure(fila, weight=1)
+        for columna in range(columnas):
+            etiqueta = Label(canvas, text=random.randint(0, 9), bg="blue")
+            etiqueta.configure(font=("Arial", 25))
+            etiqueta.grid(row=fila, column=columna, padx=2, pady=2, sticky="nsew")
+            ventana.columnconfigure(columna, weight=1)
+
+
+def hallar_numero():
+    filas = int(entry_numero.get())
+    columnas = int(entry_numero.get())
+    for numero in range(9):
+        color = random.sample(COLORS, 1)
         for fila in range(filas):
             for columna in range(columnas):
-                if etiqueta.cget("text") == i:
-                    ventana.configure(bg="cadetblue3")
+                if (
+                    canvas.grid_slaves(row=fila, column=columna)[0].cget("text")
+                    == numero
+                ):
+                    canvas.grid_slaves(row=fila, column=columna)[0].configure(
+                        background=color
+                    )
 
 
-encontrar_numero()
-
+frame = tk.Frame(ventana, background="white")
+frame.pack()
+canvas = tk.Canvas(frame, background="white")
+canvas.pack()
+entry_numero = ttk.Entry(justify=tk.RIGHT)
+entry_numero.pack()
+boton_numero = tk.Button(
+    text="Crear", command=lambda: crear_matriz(int(entry_numero.get()))
+)
+boton_numero.pack()
+boton_hallar_numero = tk.Button(text="Hallar", command=hallar_numero)
+boton_hallar_numero.pack()
 ventana.mainloop()
