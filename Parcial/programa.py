3c62f8bcd93697285874e011167ce6467ffde234
Andrés Felipe López Pinto <102700999+10Andres10@users.noreply.github.com>
2022-12-01 11:27:22 -0500

    Update programa.py

--- Parcial/programa.py
+++ Parcial/programa.py
@@ -1,31 +1,35 @@
-# Importo las librerias para llevar asi la parte  de la matriz 
-from tkinter import *
+import tkinter as tk
+from tkinter import Tk, Label
+from tkinter import ttk
 import random
- 
-# los numeros se agragan para asi poder poner el numero de filas y de colunas que tiene cada matriz 
+
 filas = 3
 columnas = 3
-
-#---------------------------
-# Funciones operaciones
-# --------------------------
 
 ventana = Tk()
 ventana.geometry("500x500")
 ventana.title("Matríz")
 
-#num = [0,1,2,3,4,5,6,7,8,9]
-
-#for i in range(1):
-#   print(random.choice(num))
+numero = ttk.Entry(justify=tk.RIGHT)
+# numero.pack()
 
 for fila in range(filas):
-   ventana.rowconfigure(fila, weight=1)
-   for columna in range(columnas):
-      etiqueta = Label(text="1"+str(fila)+str(columna), bg="cadetblue1")
-      etiqueta.grid(row=fila,column=columna, padx=2, pady=2, sticky="nsew")
-      #etiqueta.grid(row=fila,column=columna, padx=2, pady=2)
-      ventana.columnconfigure(columna, weight=1)
+    ventana.rowconfigure(fila, weight=1)
+    for columna in range(columnas):
+        etiqueta = Label(text=random.randint(0, 9), bg="cadetblue1",font=1)
+        etiqueta.configure(font=99)
+        etiqueta.grid(row=fila, column=columna, padx=2, pady=2, sticky="nsew")
+        ventana.columnconfigure(columna, weight=1)
+
+
+def encontrar_numero():
+    for i in range(0, 9):
+        for fila in range(filas):
+            for columna in range(columnas):
+                if etiqueta.cget("text") == i:
+                    ventana.configure(bg="cadetblue3")
+
+
+encontrar_numero()
 
 ventana.mainloop()
-
